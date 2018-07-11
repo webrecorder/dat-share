b07d211dbb76cc52fe5d179b5221b0e7ec2af59f
John Berlin <n0tan3rd@gmail.com>
2018-07-11 19:50:27 -0400

    init webrecorder dat intergration

---
+++ run.js
@@ -0,0 +1,13 @@
+const fastify = require('./lib/server')
+
+;(async () => {
+  try {
+    await fastify.listen(3000)
+    fastify.log.info(`server listening on ${fastify.server.address().port}`)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+})()
+
+
